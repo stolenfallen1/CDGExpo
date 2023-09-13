7e52caa2a4e1cb16eeb3401da3e3bd20226bcb9a
Paw <76894433+stolenfallen1@users.noreply.github.com>
2023-09-13 14:32:06 +0800

    navigation major adjustment

--- src/navigation/DrawerContent.jsx
+++ src/navigation/DrawerContent.jsx
@@ -1,67 +1,61 @@
 import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
-import { DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
-import { routes } from "../navData/menuItem";
-import { useState } from "react";
-import { Ionicons } from "@expo/vector-icons";
-import Icon from "react-native-vector-icons/AntDesign";
+import { useRecoilValue } from "recoil";
+import { userRoleState } from "../atoms/userRoleState";
 
 const DrawerContent = ({ navigation }) => {
-  const [expandedItems, setExpandedItems] = useState([]);
+  const userRole = useRecoilValue(userRoleState);
 
-  const toggleItem = (itemName) => {
-    if (expandedItems.includes(itemName)) {
-      setExpandedItems(expandedItems.filter((item) => item !== itemName));
+  const renderMenuItems = () => {
+    if (userRole === "administrator") {
+      return (
+        <View>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("")}
+            style={styles.menuItemButton}
+          >
+            <Text style={styles.menuItemText}>Administrator</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Login")}
+            style={styles.menuItemButton}
+          >
+            <Text style={styles.menuItemText}>Logout</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    } else if (userRole === "department head") {
+      return (
+        <View>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("")}
+            style={styles.menuItemButton}
+          >
+            <Text style={styles.menuItemText}>Department Head</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Login")}
+            style={styles.menuItemButton}
+          >
+            <Text style={styles.menuItemText}>Logout</Text>
+          </TouchableOpacity>
+        </View>
+      );
     } else {
-      setExpandedItems([...expandedItems, itemName]);
+      return (
+        <View>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Login")}
+            style={styles.menuItemButton}
+          >
+            <Text style={styles.menuItemText}>Logout</Text>
+          </TouchableOpacity>
+        </View>
+      );
     }
   };
 
-  const renderMenuItems = (menuItems, level = 0) => {
-    return menuItems.map((item) => {
-      if (item.screens) {
-        const isExpanded = expandedItems.includes(item.name);
-        return (
-          <View key={item.name}>
-            <TouchableOpacity
-              onPress={() => toggleItem(item.name)}
-              style={styles.menuItemButton}
-            >
-              <Text style={styles.menuItemText}>
-                {isExpanded ? (
-                  <Icon name="caretup" size={14} style={{ color: "blue" }} />
-                ) : (
-                  <Icon name="caretdown" size={14} style={{ color: "blue" }} />
-                )}
-              </Text>
-              <Text style={styles.menuItemText}>{item.name}</Text>
-            </TouchableOpacity>
-            {isExpanded && renderMenuItems(item.screens, level + 1)}
-          </View>
-        );
-      } else {
-        return (
-          <DrawerItem
-            key={item.name}
-            label={item.name}
-            onPress={() => navigation.navigate(item.component)}
-            labelStyle={styles.menuItemText}
-            icon={() => (
-              <Ionicons name={item.iconName} size={18} color={item.iconColor} />
-            )}
-          />
-        );
-      }
-    });
-  };
-
-  return (
-    <View style={styles.container}>
-      <DrawerContentScrollView>
-        {renderMenuItems(routes)}
-      </DrawerContentScrollView>
-    </View>
-  );
+  return <View style={styles.container}>{renderMenuItems()}</View>;
 };
 
 const styles = StyleSheet.create({
