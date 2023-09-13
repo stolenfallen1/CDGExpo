a6f7ff68d4a1d6d63ea4c5ee824b34b5b77445fe
Paw <76894433+stolenfallen1@users.noreply.github.com>
2023-09-13 17:09:34 +0800

    PR - fetch transaction history for admin and DH

--- src/navigation/NavComponents/DepartmentHeadHistory.jsx
+++ src/navigation/NavComponents/DepartmentHeadHistory.jsx
@@ -1,11 +1,50 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView } from "react-native";
+import React, { useState, useEffect } from "react";
+import { useRecoilValue } from "recoil";
+import { authTokenState } from "../../atoms/authTokenState";
+import axios from "axios";
 
 const DepartmentHeadHistory = () => {
+  const authToken = useRecoilValue(authTokenState);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://10.4.15.206:8004/api/purchase-request?page=1&per_page=10&tab=2",
+          {
+            headers: {
+              Authorization: `Bearer ${authToken}`,
+            },
+          }
+        );
+        setData(response.data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [authToken]);
+
   return (
-    <View>
-      <Text>DepartmentHeadHistory</Text>
-    </View>
+    <ScrollView>
+      {Object.keys(data).map((key) => (
+        <View key={key}>
+          <Text>{data[key].code}</Text>
+          <Text>{data[key].pr_Transaction_Date}</Text>
+          <Text>{data[key].user.name}</Text>
+          <Text>{data[key].item_group.name}</Text>
+          <Text>{data[key].category.name}</Text>
+          <Text>{data[key].code}</Text>
+          {/* <Text>
+            {data[key].purchase_request_details[key].item_Request_Qty}
+          </Text> */}
+          <Text>{data[key].pr_Justication}</Text>
+        </View>
+      ))}
+    </ScrollView>
   );
 };
 
