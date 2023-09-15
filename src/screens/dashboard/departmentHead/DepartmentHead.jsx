f9b10b360205c0bab6b774da98ccfacbd5ee9439
Paw <76894433+stolenfallen1@users.noreply.github.com>
2023-09-15 15:29:53 +0800

    PR for DH - approve nalang kulang

--- src/screens/dashboard/departmentHead/DepartmentHead.jsx
+++ src/screens/dashboard/departmentHead/DepartmentHead.jsx
@@ -1,10 +1,11 @@
 import {
   ScrollView,
   TouchableOpacity,
-  View,
-  Text,
   Button,
   StyleSheet,
+  TextInput,
+  Text,
+  View,
 } from "react-native";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
@@ -13,7 +14,8 @@ import { authTokenState } from "../../../atoms/authTokenState";
 import CardData from "../../../components/CardData";
 import SearchFilter from "../../../components/SearchFilter";
 import Modal from "react-native-modal";
-import { Card, Input } from "react-native-elements";
+import { Card } from "react-native-elements";
+import RNPickerSelect from "react-native-picker-select";
 
 const DepartmentHead = () => {
   const authToken = useRecoilValue(authTokenState);
@@ -33,12 +35,12 @@ const DepartmentHead = () => {
   };
 
   const getVendor = (id) => {
-    const vendor = vendors.find(vendor => vendor.id == id)
-    return vendor.vendor_Name
+    const vendor = vendors.find((vendor) => vendor.id == id);
+    return vendor.vendor_Name;
   };
   const getUnit = (id) => {
-    const unit = units.find(unit => unit.id == id)
-    return unit.name
+    const unit = units.find((unit) => unit.id == id);
+    return unit.name;
   };
 
   useEffect(() => {
@@ -59,14 +61,11 @@ const DepartmentHead = () => {
     };
     const fetchVendors = async () => {
       try {
-        const response = await axios.get(
-          "http://10.4.15.12:8004/api/vendors",
-          {
-            headers: {
-              Authorization: `Bearer ${authToken}`,
-            },
-          }
-        );
+        const response = await axios.get("http://10.4.15.12:8004/api/vendors", {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        });
         setVendors(response.data.data);
       } catch (error) {
         console.error(error);
@@ -74,14 +73,11 @@ const DepartmentHead = () => {
     };
     const fetchUnits = async () => {
       try {
-        const response = await axios.get(
-          "http://10.4.15.12:8004/api/units",
-          {
-            headers: {
-              Authorization: `Bearer ${authToken}`,
-            },
-          }
-        );
+        const response = await axios.get("http://10.4.15.12:8004/api/units", {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        });
         setUnits(response.data.units);
       } catch (error) {
         console.error(error);
@@ -117,19 +113,90 @@ const DepartmentHead = () => {
         <ScrollView>
           {selectedCardData?.purchase_request_details?.map((item, index) => (
             <Card key={index} containerStyle={styles.cardContainer}>
-              <Text>Item Code: {item.item_Id}</Text>
-              <Text>Item Name: {item.item_master?.item_name}</Text>
-              <Text>Preferred Supplier: {item.prepared_supplier_id?getVendor(item.prepared_supplier_id):''}</Text>
-              <Text>Quantity: {item?.item_Request_Qty}</Text>
-              <Text>UOM: {getUnit(item.item_Request_UnitofMeasurement_Id)}</Text>
-              <Text>
-                Approved Quantity: {item?.item_Request_Department_Approved_Qty}
-              </Text>
-              <Text>
-                Approved UOM:{" "}
-                {item?.item_Request_Department_Approved_UnitofMeasurement_Id}
-              </Text>
-              <Text>Action</Text>
+              <View style={styles.inputContainer}>
+                <Text style={styles.inputText}>Item Code </Text>
+                <TextInput style={styles.dataInput} editable={false}>
+                  {item.item_Id}
+                </TextInput>
+              </View>
+              <View style={styles.inputContainer}>
+                <Text style={styles.inputText}>Item Name </Text>
+                <TextInput style={styles.dataInput} editable={false}>
+                  {item.item_master?.item_name}
+                </TextInput>
+              </View>
+              <View style={styles.inputContainer}>
+                <Text style={styles.inputText}>Preferred Supplier</Text>
+                <RNPickerSelect
+                  value={item.prepared_supplier_id}
+                  onValueChange={(value) => {
+                    // Handle the event change here
+                  }}
+                  items={vendors.map((vendor) => ({
+                    label: vendor.vendor_Name,
+                    value: vendor.id,
+                  }))}
+                  placeholder={{
+                    label: getVendor(item.prepared_supplier_id),
+                    value: item.prepared_supplier_id,
+                  }}
+                />
+              </View>
+              <View style={styles.inputContainer}>
+                <Text style={styles.inputText}>Requested Quantity </Text>
+                <TextInput style={styles.dataInput} editable={false}>
+                  {parseInt(item?.item_Request_Qty)}
+                </TextInput>
+              </View>
+              <View style={styles.inputContainer}>
+                <Text style={styles.inputText}>
+                  Requested Unit of Measurement
+                </Text>
+                <RNPickerSelect
+                  disabled={true}
+                  value={item.prepared_supplier_id}
+                  onValueChange={(value) => {
+                    // Handle the event change here
+                  }}
+                  items={units.map((unit) => ({
+                    label: unit.name,
+                    value: unit.id,
+                  }))}
+                  placeholder={{
+                    label: getUnit(item.item_Request_UnitofMeasurement_Id),
+                    value: item.item_Request_UnitofMeasurement_Id,
+                  }}
+                />
+              </View>
+              <View style={styles.inputContainer}>
+                <Text style={styles.inputText}>Approved Quantity </Text>
+                <TextInput
+                  style={styles.dataInput}
+                  // {item?.item_Request_Department_Approved_Qty}
+                >
+                  {parseInt(item?.item_Request_Qty)}
+                </TextInput>
+              </View>
+              <View style={styles.inputContainer}>
+                <Text style={styles.inputText}>
+                  Approved Unit of Measurement
+                </Text>
+                <RNPickerSelect
+                  // {item?.item_Request_Department_Approved_UnitofMeasurement_Id}
+                  value={item.prepared_supplier_id}
+                  onValueChange={(value) => {
+                    // Handle the event change here
+                  }}
+                  items={units.map((unit) => ({
+                    label: unit.name,
+                    value: unit.id,
+                  }))}
+                  placeholder={{
+                    label: getUnit(item.item_Request_UnitofMeasurement_Id),
+                    value: item.item_Request_UnitofMeasurement_Id,
+                  }}
+                />
+              </View>
               <Button title="Approve" />
             </Card>
           ))}
@@ -150,6 +217,18 @@ const styles = StyleSheet.create({
   cardContainer: {
     borderRadius: 10,
     borderColor: "#66B5D1",
+  },
+  inputContainer: {
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+  },
+  inputText: {
+    fontWeight: "bold",
+    fontSize: 16,
+    marginBottom: 3,
+  },
+  dataInput: {
+    fontSize: 18,
   },
 });
 
