6a55884b0cf64e31e96a57d763c4f6ae60c6f1bb
Paw <76894433+stolenfallen1@users.noreply.github.com>
2023-10-20 14:52:43 +0800

    department head action button history transactions

--- src/navigation/PRNavigations/DHead.jsx
+++ src/navigation/PRNavigations/DHead.jsx
@@ -1,4 +1,10 @@
-import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ActivityIndicator,
+  ScrollView,
+} from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 import React, { useState, useEffect } from "react";
 import { useRecoilValue } from "recoil";
@@ -10,7 +16,9 @@ import PRCard from "../../components/Cards/PRCard";
 import { customStyles } from "../../styles/customStyles";
 import { Ionicons } from "@expo/vector-icons";
 import Modal from "react-native-modal";
+import { Button, Card } from "react-native-elements";
 import ModalFilter from "../../components/ModalFilter";
+import ModalHeader from "../../components/Modals/PRModalHeader";
 
 const apiKey = process.env.EXPO_PUBLIC_API_URL;
 
@@ -20,16 +28,39 @@ const DepartmentHeadHistory = () => {
   const branchID = useRecoilValue(userBranchID);
   // Data states
   const [data, setData] = useState([]);
+  const [selectedCardData, setSelectedCardData] = useState({});
+  const [vendors, setVendors] = useState([]);
+  const [units, setUnits] = useState([]);
   // Modal states
   const [filterModal, setFilterModal] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
   // Filter states
   const [selectedCategory, setSelectedCategory] = useState("");
   const [selectedItemGroup, setSelectedItemGroup] = useState("");
   // Loading states
   const [isLoading, setIsLoading] = useState(false);
 
+  const handleCardPress = (cardData) => {
+    setSelectedCardData({ ...cardData });
+    toggleModal();
+  };
+
   const toggleFilter = () => {
     setFilterModal(true);
+  };
+
+  const toggleModal = () => {
+    setModalVisible(!modalVisible);
+  };
+
+  const getVendor = (id) => {
+    const vendor = vendors.find((vendor) => vendor.id == id);
+    return vendor?.vendor_Name ? vendor.vendor_Name : "";
+  };
+
+  const getUnit = (id) => {
+    const unit = units.find((unit) => unit.id == id);
+    return unit?.name;
   };
 
   const handleFilterApply = ({ category, item_group }) => {
@@ -38,28 +69,74 @@ const DepartmentHeadHistory = () => {
     setFilterModal(false);
   };
 
-  useEffect(() => {
+  const fetchData = async () => {
     setIsLoading(true);
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `${apiKey}/purchase-request?page=1&per_page=10&tab=2&branch=${branchID}&item_group=${selectedItemGroup}&category=${selectedCategory}`,
-          {
-            headers: {
-              Authorization: `Bearer ${authToken}`,
-            },
-          }
-        );
-        setData(response.data.data);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+    try {
+      const response = await axios.get(
+        `${apiKey}/purchase-request?page=1&per_page=10&tab=2&branch=${branchID}&item_group=${selectedItemGroup}&category=${selectedCategory}`,
+        {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      );
+      setData(response.data.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  const fetchVendors = async () => {
+    try {
+      const response = await axios.get(`${apiKey}/vendors`, {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+      console.log(response.data.data);
+      setVendors(response.data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const fetchUnits = async () => {
+    try {
+      const response = await axios.get(`${apiKey}/units`, {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+      setUnits(response.data.units);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
+  useEffect(() => {
     fetchData();
+    fetchVendors();
+    fetchUnits();
   }, [authToken, branchID, selectedItemGroup, selectedCategory]);
+
+  const renderItem = ({ item }) => {
+    return (
+      <TouchableOpacity onPress={() => handleCardPress(item)}>
+        <PRCard
+          prId={item?.pr_Document_Number}
+          transactionDate={item?.pr_Transaction_Date}
+          requestingName={item?.pr_RequestedBy}
+          warehouse={item?.warehouse?.warehouse_description}
+          itemGroup={item?.item_group?.name}
+          category={item?.category?.name}
+          quantity={item?.purchase_request_details?.item_Request_Qty}
+          pr_status={item?.status?.Status_description}
+          dateApproved={item?.pr_DepartmentHead_ApprovedDate}
+          justification={item?.pr_Justication}
+        />
+      </TouchableOpacity>
+    );
+  };
 
   return (
     <View style={{ paddingBottom: 110 }}>
@@ -86,30 +163,61 @@ const DepartmentHeadHistory = () => {
         <Text style={customStyles.emptyText}>No results found</Text>
       ) : (
         <FlatList
-          data={Object.keys(data)}
-          keyExtractor={(key) => key}
-          renderItem={({ item }) => {
-            return (
-              <TouchableOpacity>
-                <PRCard
-                  prId={data[item]?.pr_Document_Number}
-                  transactionDate={data[item]?.pr_Transaction_Date}
-                  requestingName={data[item]?.pr_RequestedBy}
-                  warehouse={data[item]?.warehouse?.warehouse_description}
-                  itemGroup={data[item]?.item_group?.name}
-                  category={data[item]?.category?.name}
-                  quantity={
-                    data[item]?.purchase_request_details?.item_Request_Qty
-                  }
-                  pr_status={data[item]?.status?.Status_description}
-                  dateApproved={data[item]?.pr_DepartmentHead_ApprovedDate}
-                  justification={data[item]?.pr_Justication}
-                />
-              </TouchableOpacity>
-            );
-          }}
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={renderItem}
         />
       )}
+      <Modal isVisible={modalVisible} style={customStyles.modalContainer}>
+        <View style={{ marginLeft: 16, marginTop: 15 }}>
+          <ModalHeader
+            prNum={selectedCardData?.pr_Document_Number}
+            name={selectedCardData?.user?.name}
+            warehouse={selectedCardData?.warehouse?.warehouse_description}
+            requestedBy={selectedCardData?.user?.name}
+            dateRequested={new Date(
+              selectedCardData?.pr_Transaction_Date
+            ).toLocaleDateString()}
+          />
+        </View>
+        <ScrollView style={{ borderTopWidth: 1, borderTopColor: "lightgrey" }}>
+          {selectedCardData?.purchase_request_details?.map((item, index) => (
+            <Card key={index} containerStyle={customStyles.cardContainer}>
+              <View style={customStyles.inputContainer}>
+                <Text style={customStyles.inputText}>Item Name: </Text>
+                <Text style={customStyles.dataInput}>
+                  {item?.item_master?.item_name}
+                </Text>
+              </View>
+              <View style={customStyles.inputContainer}>
+                <Text style={customStyles.inputText}>Preferred Supplier: </Text>
+                <Text style={customStyles.dataInput}>
+                  {getVendor(item?.prepared_supplier_id)}
+                </Text>
+              </View>
+              <View style={customStyles.inputContainer}>
+                <Text style={customStyles.inputText}>Quantity: </Text>
+                <Text style={customStyles.dataInput}>
+                  {item?.item_Request_Department_Approved_Qty}
+                </Text>
+              </View>
+              <View style={customStyles.inputContainer}>
+                <Text style={customStyles.inputText}>Unit of Measurement:</Text>
+                <Text style={customStyles.dataInput}>
+                  {getUnit(
+                    item?.item_Request_Department_Approved_UnitofMeasurement_Id
+                  )}
+                </Text>
+              </View>
+            </Card>
+          ))}
+        </ScrollView>
+        <Button
+          title={"Back"}
+          buttonStyle={customStyles.cancelButton}
+          onPress={toggleModal}
+        />
+      </Modal>
     </View>
   );
 };
